45ad0c0abe2617641594b1043297f84482c411d0
Lambda_School_Loaner_182 <lambda_school_loaner_182@Lambda-School-Loaner-182s-MacBook-Pro.local>
2020-08-12 08:56:43 -0400

    save

--- router/data-router.js
+++ router/data-router.js
@@ -1,14 +1,16 @@
 const express = require('express');
 
 const data = require('./data-model')
-
+const stripe = require("stripe")("sk_test_51HF9J6FriexrfnPA74CdV2Xrps7HzN7goZPONlSKSPMru1VUgcEDSj56LZVGcKZaxIyhOzzmSl7xJ44xdNHNFduE005cJaQTXm");
 const router = express.Router();
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const multer = require("multer")
-
+const cors = require("cors")
+const uuid = require("uuid/v4");
 // router.use(cors({ origin: "*" }));
 // router.use(bodyParser.json());
+router.use(cors());
 
 router.post('/register', (req, res) => {
   console.log(req.body)
@@ -80,6 +82,50 @@ router.get('/orders', (req, res) => {
     res.status(500).json({ message: 'Failed to get schemes' });
   });
 })
+router.post("/checkout", (req, res) => {
+ 
 
+  
+    const { product, token } = req.body;
+    console.log("Request:", product);
+    console.log("price:", product.price);
+    const idempotencyKey = uuid();
+    return stripe.customers.create({
+      email: token.email,
+      source: token.id
+    }).then(
+      customer => {
+        stripe.charges.create(
+          {
+            amount: product.price * 100,
+            currency: "usd",
+            customer: customer.id,
+            receipt_email: token.email,
+            description: `Purchased the ${product.name}`,
+            shipping: {
+              name: token.card.name,
+              address: {
+                line1: token.card.address_line1,
+                line2: token.card.address_line2,
+                city: token.card.address_city,
+                country: token.card.address_country,
+                postal_code: token.card.address_zip
+              }
+            }
+          },
+          {
+            idempotencyKey
+          }
+        );
+      }
+    )
+    .then(project => {
+      console.log("usd")
+      res.status(200).json(project)})
+    .catch(err => {
+      res.status(500).json(console.log(err));
+    });
+ 
+})
 
 module.exports = router
